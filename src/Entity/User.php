c7f763d1f74dc82a98aee95c1159bb4608a88ee6
NishantSoni <soninishant287@gmail.com>
2018-04-16 22:52:27 +0530

    some changes

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,8 +17,30 @@ class User extends BaseUser
      */
     protected $id;
 
-    // add your own fields
+    /** @ORM\Column(name="github_id", type="string", length=255, nullable=true) */
+    protected $github_id;
 
+    /** @ORM\Column(name="github_access_token", type="string", length=255, nullable=true) */
+    protected $github_access_token;
+
+    /** @ORM\Column(name="facebook_id", type="string", length=255, nullable=true) */
+    protected $facebook_id;
+
+    /** @ORM\Column(name="facebook_access_token", type="string", length=255, nullable=true) */
+    protected $facebook_access_token;
+
+    /** @ORM\Column(name="googleplus_id", type="string", length=255, nullable=true) */
+    protected $googleplus_id;
+
+    /** @ORM\Column(name="googleplus_access_token", type="string", length=255, nullable=true) */
+    protected $googleplus_access_token;
+
+    /** @ORM\Column(name="stackexchange_id", type="string", length=255, nullable=true) */
+    protected $stackexchange_id;
+
+    /** @ORM\Column(name="stackexchange_access_token", type="string", length=255, nullable=true) */
+    protected $stackexchange_access_token;
+    
     /**
      * Get id
      *
@@ -27,5 +49,91 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    public function setGithubId($githubId)
+    {
+        
+        $this->github_id = $githubId;
+
+        return $this;
+    }
+
+    public function getGithubId() {
+        return $this->github_id;
+    }
+
+    public function setGithubAccessToken($githubAccessToken)
+    {
+        
+        $this->github_access_token = $githubAccessToken;
+
+        return $this;
+    }
+
+    public function getGithubAccessToken()
+    {
+        return $this->github_access_token;
+    }
+
+    public function setFacebookId($facebookID) {
+        $this->facebook_id = $facebookID;
+
+        return $this;
+    }
+
+    public function getFacebookId() {
+        return $this->facebook_id;
+    }
+
+    public function setFacebookAccessToken($facebookAccessToken) {
+        $this->facebook_access_token = $facebookAccessToken;
+
+        return $this;
+    }
+
+    public function getFacebookAccessToken() {
+        return $this->facebook_access_token;
+    }
+
+    public function setGoogleplusId($googlePlusId) {
+        $this->googleplus_id = $googlePlusId;
+
+        return $this;
+    }
+
+    public function getGoogleplusId() {
+        return $this->googleplus_id;
+    }
+
+    public function setGoogleplusAccessToken($googleplusAccessToken) {
+        $this->googleplus_access_token = $googleplusAccessToken;
+
+        return $this;
+    }
+
+    public function getGoogleplusAccessToken() {
+        return $this->googleplus_access_token;
+    }
+
+
+    public function setStackexchangeId($stackExchangeId) {
+        $this->stackexchange_id = $stackExchangeId;
+
+        return $this;
+    }
+
+    public function getStackexchangeId() {
+        return $this->stackexchange_id;
+    }
+
+    public function setStackexchangeAccessToken($stackExchangeAccessToken) {
+        $this->stackexchange_access_token = $stackExchangeAccessToken;
+
+        return $this;
+    }
+
+    public function getStackexchangeAccessToken() {
+        return $this->stackexchange_access_token;
     }
 }
---
+++ src/Migrations/Version20180312182046.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180312182046 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE post ADD created_at DATETIME DEFAULT NULL, DROP create_at, CHANGE title title LONGTEXT NOT NULL, CHANGE updated_at updated_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE post ADD create_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, DROP created_at, CHANGE title title VARCHAR(1000) NOT NULL COLLATE utf8_unicode_ci, CHANGE updated_at updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+    }
+}
